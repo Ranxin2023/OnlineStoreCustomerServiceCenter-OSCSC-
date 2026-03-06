1302f658f79b317592ee2fa79d74f30950041d4a
RanxinLi <ranxinli2024@gmail.com>
2026-03-06 00:32:19 -0800

    finish setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,14 @@
 @echo off
-echo LOCALHOST_URL=http://localhost:5000 >> .env
-echo CHROME_PATH=C:\Program Files\Google\Chrome\Application\chrome.exe>> .env
-echo CHROME_USER_DATA=C:\Users\%USERNAME%\chrome-selenium >> .env
-echo BASE_URL= "https://csp.aliexpress.com" >> .env
-echo DEBUG_PORT=9222 >> .env
-echo "Finish setting up"
+
+echo Creating .env file...
+
+(
+echo LOCALHOST_URL=http://localhost:5001
+echo CHROME_PATH=C:\Program Files\Google\Chrome\Application\chrome.exe
+echo CHROME_USER_DATA=C:\Users\%USERNAME%\chrome-selenium
+echo BASE_URL=https://csp.aliexpress.com
+echo DEBUG_PORT=9222
+) > OnlineStoreCustomerServiceCenter-OSCSC--main/.env
+
+echo Finish setting up
+pause
